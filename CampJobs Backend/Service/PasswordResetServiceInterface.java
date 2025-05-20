a04ade7b0795c1e2958c2671424685e9c6a82521
Parkacho Ismail Khamis <123508777+Parkacho-Ismail@users.noreply.github.com>
2025-05-20 13:34:12 +0300

    Update PasswordResetServiceInterface.java

--- CampJobs Backend/Service/PasswordResetServiceInterface.java
+++ CampJobs Backend/Service/PasswordResetServiceInterface.java
@@ -3,7 +3,7 @@ package com.example.campsjobs.Service;
 public interface PasswordResetServiceInterface {
     String generateResetToken(String email);
     String resetPassword(String token, String newPassword);
-    boolean validateToken(String token); // Add this method
+    boolean validateToken(String token); 
 
 }
 
